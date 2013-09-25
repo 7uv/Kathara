0864cd7a4d336d069ed890800e89d40c5fbbbb00
sayantan <sayantan7@gmail.com>
2013-09-25 20:52:34 +0200

    base project
    struts2 + hibernate - spring - jpa
    TODO:
    build
    add spring
    add jpa

---
+++ str/src/com/sayantan/action/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @description
+ * Contains action classes for various controls.
+ * May be create separate packages for separate action groups 
+ * but only when the project becomes too big
+ */
+/**
+ * @author zayantan
+ *
+ */
+package com.sayantan.action;
